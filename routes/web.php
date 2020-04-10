e53e905dab0260e94b3cf3b5e16a654b282fe04c
Iszuddin Ismail <dino@websegera.my>
2020-04-10 17:57:51 +0800

    update routing
    Signed-off-by: Iszuddin Ismail <dino@websegera.my>

--- routes/web.php
+++ routes/web.php
@@ -17,7 +17,7 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::redirect('/', '/admin/login');
+// Route::redirect('/', '/admin/login');
 
 Route::group(['prefix' => 'admin'], function () {
     Voyager::routes();

a5ae8d2c897e3d58fa93c86974ee1e0c31171867
Iszuddin Ismail <dino@websegera.my>
2020-04-10 17:25:03 +0800

    redirect home

--- routes/web.php
+++ routes/web.php
@@ -13,10 +13,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+//Route::get('/', function () {
+//    return view('welcome');
+//});
 
+Route::redirect('/', '/admin/login');
 
 Route::group(['prefix' => 'admin'], function () {
     Voyager::routes();
